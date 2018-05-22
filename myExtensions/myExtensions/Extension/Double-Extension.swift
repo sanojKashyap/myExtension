093a14d78c9288ef716e9fd56018a47780331adf
Sanoj Kashyap <sanojkashyap@sanojs-macbook.symphonyteleca.com>
2018-05-22 15:41:04 +0530

    intial commit

---
+++ myExtensions/myExtensions/Extension/Double-Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Double-Extension.swift
+//  myExtensions
+//
+//  Created by Sanoj Kashyap on 22/05/18.
+//  Copyright © 2018 Sanoj Kashyap. All rights reserved.
+//
+
+import Foundation
---
+++ myExtensions/myExtensions/myExtensions.h
@@ -0,0 +1,19 @@
+//
+//  myExtensions.h
+//  myExtensions
+//
+//  Created by Sanoj Kashyap on 22/05/18.
+//  Copyright © 2018 Sanoj Kashyap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myExtensions.
+FOUNDATION_EXPORT double myExtensionsVersionNumber;
+
+//! Project version string for myExtensions.
+FOUNDATION_EXPORT const unsigned char myExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myExtensions/PublicHeader.h>
+
+
